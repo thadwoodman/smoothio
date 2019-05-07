da949e9b404dca0787dd0846f55b0fe0ef7b2d26
thadwoodman <thadwoodman@gmail.com>
2019-05-07 13:42:13 -0400

    add and save ingredients

---
+++ src/components/SmoothieIngredient.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="smoothie-ingredient">{{name}}</div>
+</template>
+
+<script>
+import { db } from "@/db";
+
+export default {
+  name: "SmoothieIngredient",
+  props: ["name"]
+};
+</script>
--- src/views/Smoothie.vue
+++ src/views/Smoothie.vue
@@ -1,31 +1,69 @@
 <template>
   <div class="smoothie">
     <header>
-      <h1 class="smoothie-name">{{smoothieName}}</h1>
+      <h1 class="smoothie-name">{{smoothie.name}}</h1>
     </header>
+    <div class="smoothie-ingredients">
+      <SmoothieIngredient
+        v-for="ingredient in smoothie.ingredients"
+        v-bind:key="ingredient.id"
+        v-bind:name="ingredient.name"
+        class="content"
+      />
+    </div>
+    <div class="add-smoothie-ingredients">
+      <button v-on:click="showIngredientsForm = true">Add ingredients</button>
+      <form v-if="showIngredientsForm" v-on:submit="addIngredient">
+        <input type="text" v-model="newIngredientInput">
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
 import { db } from "@/db";
+import SmoothieIngredient from "@/components/SmoothieIngredient.vue";
 
 export default {
   name: "smoothie",
+  components: { SmoothieIngredient },
   data() {
     return {
-      smoothieName: ""
+      smoothieID: "",
+      smoothie: {},
+      showIngredientsForm: false,
+      newIngredientInput: ""
     };
   },
   created() {
     // load smoothie on create
-    // retrieve a document
-    db.collection("smoothies")
-      .doc(this.$route.params.smoothieID)
-      .get()
-      .then(snapshot => {
-        const document = snapshot.data();
-        this.smoothieName = document.name;
-      });
+    this.smoothieID = this.$route.params.smoothieID;
+    this.fetchSmoothieData();
+  },
+  methods: {
+    fetchSmoothieData() {
+      db.collection("smoothies")
+        .doc(this.smoothieID)
+        .get()
+        .then(snapshot => {
+          this.smoothie = snapshot.data();
+        });
+      s;
+    },
+    addIngredient(event) {
+      event.preventDefault();
+      if (!this.smoothie.ingredients) {
+        this.smoothie.ingredients = [];
+      }
+      this.smoothie.ingredients.push({ name: this.newIngredientInput });
+      this.updateSmoothie();
+    },
+    updateSmoothie() {
+      db.collection("smoothies")
+        .doc(this.smoothieID)
+        .set(this.smoothie);
+      this.showIngredientsForm = false;
+    }
   }
 };
 </script>
