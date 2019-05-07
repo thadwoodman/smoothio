9603ebf8e0e638603127905fa8e535623e819f10
thadwoodman <thadwoodman@gmail.com>
2019-05-07 14:25:13 -0400

    add smoothie uniqueness and remove smoothie

---
+++ src/components/SmoothieListItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="smoothie-list-item">
+    {{name}}
+    <button v-on:click="removeSmoothie">Remove</button>
+  </div>
+</template>
+
+<script>
+import { db } from "@/db";
+
+export default {
+  name: "SmoothieListItem",
+  props: ["name", "id"],
+  methods: {
+    removeSmoothie() {
+      db.collection("smoothies")
+        .doc(this.$props.id)
+        .delete();
+    }
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,41 +9,68 @@
         <form v-on:submit="onAddSmoothieSubmit">
           <label>Smoothie Name</label>
           <input type="text" v-model="newSmoothieName">
+          <div v-if="showNameInputError">{{this.nameInputErrorText}}</div>
         </form>
       </div>
     </div>
-    <div class="list-of-smoothies"></div>
+    <div class="list-of-smoothies">
+      <SmoothieListItem
+        v-for="smoothie in smoothies"
+        v-bind:key="smoothie.id"
+        v-bind:id="smoothie.id"
+        v-bind:name="smoothie.name"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 import { db } from "@/db";
+import SmoothieListItem from "@/components/SmoothieListItem";
 
 export default {
   name: "home",
+  components: { SmoothieListItem },
   data() {
     return {
       smoothies: [],
       isAddingSmoothie: false,
-      newSmoothieName: ""
+      newSmoothieName: "",
+      showNameInputError: false,
+      nameInputErrorText: ""
     };
   },
   methods: {
     onAddSmoothieSubmit(event) {
       event.preventDefault();
-      db.collection("smoothies")
-        .add({
-          name: this.newSmoothieName
-        })
-        .then(response => {
-          this.$router.push({
-            name: "smoothie",
-            params: { smoothieID: response.id }
+      if (this.isSmoothieUnique()) {
+        db.collection("smoothies")
+          .add({
+            name: this.newSmoothieName
+          })
+          .then(response => {
+            // navigate to newly created smoothie name
+            this.$router.push({
+              name: "smoothie",
+              params: { smoothieID: response.id }
+            });
           });
-        });
+      } else {
+        this.displayNameInputError(
+          `"${this.newSmoothieName}" has already been taken`
+        );
+      }
+    },
+    isSmoothieUnique() {
+      return !this.smoothies
+        .map(smoothie => smoothie.name)
+        .includes(this.newSmoothieName);
+    },
+    displayNameInputError(text) {
+      this.showNameInputError = true;
+      this.nameInputErrorText = text;
     }
   },
-
   firestore: {
     smoothies: db.collection("smoothies")
   }
