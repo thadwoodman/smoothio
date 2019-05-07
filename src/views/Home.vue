a95aa348cceea489a604bb8372a55802cf7b533d
thadwoodman <thadwoodman@gmail.com>
2019-05-07 12:11:20 -0400

    add input to home and wire up firebase

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,15 @@
     <header>
       <h1>Smoothio</h1>
     </header>
+    <div class="add-a-smoothie-container">
+      <button v-on:click="isAddingSmoothie = !isAddingSmoothie">Add a smoothie</button>
+      <div v-if="isAddingSmoothie" class="addSmoothieForm">
+        <form v-on:submit="onAddSmoothieSubmit">
+          <label>Smoothie Name</label>
+          <input type="text" v-model="newSmoothieName">
+        </form>
+      </div>
+    </div>
     <div class="list-of-smoothies"></div>
   </div>
 </template>
@@ -16,8 +25,17 @@ export default {
   name: "home",
   data() {
     return {
-      smoothies: []
+      smoothies: [],
+      isAddingSmoothie: false,
+      newSmoothieName: ""
     };
+  },
+  methods: {
+    onAddSmoothieSubmit() {
+      db.collection("smoothies").add({
+        name: this.newSmoothieName
+      });
+    }
   },
 
   firestore: {
